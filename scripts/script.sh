e7881856d16f123e2436e53bf0e822ac76d0ffff
gilbert215 <urinzwenimanagilbert39@gmail.com>
2025-08-11 12:34:23 +0200

    Commented lines

--- scripts/script.sh
+++ scripts/script.sh
@@ -18,5 +18,5 @@
 #python -m topology.train_topo --name abc --batch_size 512 --option faceEdge --train_epochs 1000
 # python -m topology.train_topo --name abc --batch_size 52 --option edgeVert --train_epochs 100 #--save_epochs 100
 python -m geometry.train_geom --name abc --batch_size 512 --option faceBbox --train_epochs 2000
-python -m geometry.train_geom --name abc --batch_size 512 --option vertGeom --train_epochs 2000
-python -m geometry.train_geom --name abc --batch_size 512 --option edgeGeom --train_epochs 2000
+# python -m geometry.train_geom --name abc --batch_size 512 --option vertGeom --train_epochs 2000
+# python -m geometry.train_geom --name abc --batch_size 512 --option edgeGeom --train_epochs 2000
